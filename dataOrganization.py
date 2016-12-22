4119ae1c041da4f1a5960624820ec6bc2a6f55a2
ckhoarmnp <sorapol57@gmail.com>
2016-12-22 19:21:20 +0700

    Build noob arff

--- dataOrganization.py
+++ dataOrganization.py
@@ -3,6 +3,40 @@ import pandas as pd
 
 
 
+def MakeWeka(movie_num):
+	col_list = movie_num.columns
+	col_list_type = {}
+	for col in col_list:
+		col_list_type[col] = 'numeric'
+	# print col_list_type
+
+	foo = open('train.arff', 'w+')
+	foo.write('@relation movieGross\n\n')
+	for col in col_list:
+		if(col != 'gross'):
+			if(col != col_list[-1]):
+				foo.write('@attribute ' + col + ' ' + col_list_type[col] + '\n')
+			else:
+				foo.write('@attribute ' + col + ' ' + col_list_type[col] + '\n')
+				foo.write('@attribute gross ' + col_list_type[col] + '\n')
+
+	foo.write('\n@data\n')
+	for index, row in movie_num.iterrows():
+		for col in col_list:
+
+			if (col != 'gross'):
+				foo.write(str(row[col]))
+
+			if(col != col_list[-1]):
+				if (col != 'gross'):
+					foo.write(',')
+			else:
+				foo.write(',' + str(row['gross']) + '\n')
+	foo.close()
+
+	return 0
+
+
 def main():
 	movie = pd.read_csv('mov_mm_train.csv') ### reads the csv and creates the dataframe called movie
 	#print movie.head()
@@ -10,7 +44,7 @@ def main():
 	str_list = [] ### empty list to contain columns with strings (words)
 	for colname, colvalue in movie.iteritems():
 	    if type(colvalue[1]) == str:
-	         str_list.append(colname)
+	        str_list.append(colname)
 
 	### Get to the numeric columns by inversion            
 	num_list = movie.columns.difference(str_list)
@@ -20,7 +54,11 @@ def main():
 
 	# movie_num = movie_num.dropna(subset=['gross','num_critic_for_reviews','num_user_for_reviews','num_voted_users','movie_facebook_likes'])
 	movie_num = movie_num.dropna()
-	print len(movie_num)
+
+	# How to select everything except 'gross' <<<<<<< work here
+	movie_num[~'gross'] = (movie_num[~'gross'] - movie_num[~'gross'].mean()) / (movie_num[~'gross'].max() - movie_num[~'gross'].min())
+	MakeWeka(movie_num)
+
 
 if __name__ == '__main__':
     main()
