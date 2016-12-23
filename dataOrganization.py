48ce26307c59fc7f371fff55ff7807516c9da8b2
ckhoarmnp <sorapol57@gmail.com>
2016-12-23 14:28:30 +0700

    Normalize data but still didnt change gross money

--- dataOrganization.py
+++ dataOrganization.py
@@ -40,7 +40,6 @@ def MakeWeka(movie_num):
 def main():
 	movie = pd.read_csv('mov_mm_train.csv') ### reads the csv and creates the dataframe called movie
 	#print movie.head()
-
 	str_list = [] ### empty list to contain columns with strings (words)
 	for colname, colvalue in movie.iteritems():
 	    if type(colvalue[1]) == str:
@@ -49,14 +48,14 @@ def main():
 	### Get to the numeric columns by inversion            
 	num_list = movie.columns.difference(str_list)
 	#print num_list
-
 	movie_num = movie[num_list]
 
 	# movie_num = movie_num.dropna(subset=['gross','num_critic_for_reviews','num_user_for_reviews','num_voted_users','movie_facebook_likes'])
 	movie_num = movie_num.dropna()
-
-	# How to select everything except 'gross' <<<<<<< work here
-	movie_num[~'gross'] = (movie_num[~'gross'] - movie_num[~'gross'].mean()) / (movie_num[~'gross'].max() - movie_num[~'gross'].min())
+	for col in movie_num:
+		if(col != 'gross'):
+			movie_num[col] = (movie_num[col] - movie_num[col].mean()) / (movie_num[col].max() - movie_num[col].min())
+		
 	MakeWeka(movie_num)
 
 
